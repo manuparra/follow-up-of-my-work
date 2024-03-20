16d11182881719918447cce947fc4b39b22e88ab
Manuel Parra-Royón <manuparra@gmail.com>
2024-03-20 13:06:28 +0100

    Update addtodo.sh

--- addtodo.sh
+++ addtodo.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Set the project name and description variables
+project_name="$1"
+description="$2"
+estimated="$3"
+
+# Get the current datetime
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Append the project name, description, and datetime to the tasks.csv file
+echo "$datetime,$project_name,$description,$estimated" >> todo.csv
+
+# Commit the changes to the Git repository
+git add todo.csv
+git commit -m "Added task: $datetime - $project_name - $description"
+
+# Push the changes to the remote Git repository
+git push
